6dc907e26e5a1b6792add44bac7a28541fd798e2
j-o-o <joooooo308@gmail.com>
2020-12-14 14:43:44 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useSpring, animated } from "react-spring";
+import { useDrag, useGesture } from "react-use-gesture";
+
+const items = [1, 2, 3, 4];
+const interp = (i) => (wheeled) =>
+  `translate3d(0, ${
+    200 * Math.sin(wheeled / 400 + (i * 100 * Math.PI) / 500)
+  }px, ${200 * Math.cos(wheeled / 400 + (i * 100 * Math.PI) / 500)}px)`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [{ wheeled }, setWheel] = useSpring(() => ({ wheeled: 0 }));
+  useGesture(
+    {
+      onWheel: ({ offset: [, y] }) => {
+        setWheel({
+          wheeled: y,
+        });
+      },
+    },
+    { domTarget: window }
   );
+  return items.map((i) => (
+    <animated.div
+      key={i}
+      className="whirler"
+      // style={{ translateY: posY, translateZ: posZ }}
+      style={{
+        transform: wheeled.interpolate(interp(i)),
+      }}
+    >
+      {wheeled.interpolate(interp(i))}
+    </animated.div>
+  ));
 }
 
 export default App;
